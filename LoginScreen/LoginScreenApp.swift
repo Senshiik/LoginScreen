abd49ccbc73e2e815ba90746e8c2d8743b7b5c33
Senshiik <99083828+Senshiik@users.noreply.github.com>
2022-08-04 22:41:42 +0300

    Initial Commit

---
+++ LoginScreen/LoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenApp.swift
+//  LoginScreen
+//
+//  Created by Anton Demkin on 04.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
